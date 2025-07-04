35c943ce88fd80a1ef360c37df115e9e4ea3a66f
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-04 17:10:23 +0330

    optimized reading arguments from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(&env::args().collect::<Vec<String>>());
+    let contents = fs::read_to_string(&config.file_path)
+                            .expect("Something went wrong reading the file");
+    println!("Text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
