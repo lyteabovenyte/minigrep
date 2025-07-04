4b19ab09545de329437a292bc95e0f15b7f16b65
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-04 18:47:29 +0330

    Test Driven Development while implementing search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,17 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("Text:\n{}", content);
+
+    if search(&content, &config.query).is_empty() {
+        println!("No matches found for query: {}", config.query);
+        return Ok(());
+    }
+    
+    for (index, line) in content.lines().enumerate() {
+        if line.contains(&config.query) {
+            println!("Found match at line {}: {}", index+1, line);
+        }
+    }
     Ok(())
 }
 
@@ -20,5 +30,32 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
+    }
+}
+
+// TODO: could be better with iterators.
+fn search<'a> (query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut v: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            v.push(line);
+        }
+    }
+    v
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], 
+                   search(query, content));
     }
 }
