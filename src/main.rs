50710cdf5926a4606c0b3933513dd937025dcd2c
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-06 18:24:02 +0330

    stderr and stdout + final test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::process;
 fn main() {
     let config = Config::build(&env::args().collect::<Vec<String>>()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err); // standard error stream.
         process::exit(1);
     });
 
